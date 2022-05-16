cf8cf5fcdeb91c6d830dc64129d95015ada2b46f
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-16 06:06:48 +0100

    Initial Commit

---
+++ basic-types/target/debug/deps/basic_types-35eb4eb1bae3a1e5.d
@@ -0,0 +1,5 @@
+/home/alexander/ultron/practi_code/rust_lang/rust_progarmming/basic-types/target/debug/deps/basic_types-35eb4eb1bae3a1e5.rmeta: src/main.rs
+
+/home/alexander/ultron/practi_code/rust_lang/rust_progarmming/basic-types/target/debug/deps/basic_types-35eb4eb1bae3a1e5.d: src/main.rs
+
+src/main.rs:
