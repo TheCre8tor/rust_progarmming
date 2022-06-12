f5e4647ab2d65ba27b528d6a89a2f135e900df92
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-12 05:42:54 +0100

    Added gcd function

--- chapter_2/iron_greatset-common-divisor/src/main.rs
+++ chapter_2/iron_greatset-common-divisor/src/main.rs
@@ -4,6 +4,8 @@ extern crate mime;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
 
 fn main() {
     let mut router = Router::new();
@@ -31,4 +33,65 @@ fn get_form(_request: &mut Request) -> IronResult<Response> {
     );
 
     Ok(response)
+}
+
+fn post_gcd(request: &mut Request) -> IronRequest<Response> {
+    let mut response = Response::new();
+
+    let form_data = match request.get_ref::<UrlEncodedBody>() {
+        Ok(map) => map,
+        Err(error) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Error parsing form data: {:?}\n", error));
+            return Ok(response);
+        }
+    };
+
+    let unparsed_numbers = match form_data.get("n") {
+        Some(nums) => nums,
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("form data has no 'n' parameter\n"));
+            return Ok(response);
+        }
+    };
+
+    let mut numbers = Vec::new();
+
+    for unparsed in unparsed_numbers {
+        match u64::from_str(&unparsed) {
+            Ok(n) => numbers.push(n),
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!(
+                    "Value for 'n' parameter not a number: {:?}\n",
+                    unparsed
+                ));
+                return Ok(response);
+            }
+        }
+    }
+
+    let mut d = numbers[0];
+
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+
+            m = n;
+            n = t;
+        }
+
+        m = m % n;
+    }
+
+    n
 }
