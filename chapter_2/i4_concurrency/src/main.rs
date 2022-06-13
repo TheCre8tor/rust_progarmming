89c614efa7611d3ee4f31a58b61bddc33f838bca
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-13 09:25:02 +0100

    Wrote test for parse_complex function

--- chapter_2/i4_concurrency/src/main.rs
+++ chapter_2/i4_concurrency/src/main.rs
@@ -83,13 +83,13 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
-// #[test]
-// fn test_parse_complex() {
-//     assert_eq!(
-//         parse_complex("1.25,-0.0625"),
-//         Some(Complex {
-//             re: 1.25,
-//             im: -0.0625
-//         })
-//     );
-// }
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+}
