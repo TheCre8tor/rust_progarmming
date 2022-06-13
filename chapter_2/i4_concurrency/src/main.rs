fd6b215020901ae1c3ffa0adabe142c58967c5df
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-13 13:19:34 +0100

    use assert macro from standard libary

--- chapter_2/i4_concurrency/src/main.rs
+++ chapter_2/i4_concurrency/src/main.rs
@@ -1,5 +1,5 @@
 use num::Complex;
-use std::str::FromStr;
+use std::{assert, str::FromStr};
 
 fn main() {
     println!("Hello, world!");
@@ -72,6 +72,13 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     }
 }
 
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+///
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+/// The `upper_left` and `lower_right` parameters are points on the complex
+/// plane designating the area our image covers.
 fn pixel_to_point(
     bounds: (usize, usize),
     pixel: (usize, usize),
@@ -88,6 +95,32 @@ fn pixel_to_point(
         im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
         // Why subtraction here? pixel.1 increases as we go down,
         // but the imaginary component increases as we go up.
+    }
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`
+/// which holds one grayscale pixel per byte. The `upper_left` and `lower_right`
+/// arguments specify points on the complex plane corresponding to the upper-
+/// left and lower-right corners of the pixel buffer.
+fn render(
+    bounds: (usize, usize),
+    pixels: &mut [u8],
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                Some(count) => 255 - count as u8,
+                None => 0,
+            };
+        }
     }
 }
 
