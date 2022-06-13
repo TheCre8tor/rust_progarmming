7bce861ba33c4f2d5ce65529d1723aaa679939c5
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-13 08:27:41 +0100

    Added escape_time function

--- chapter_2/i4_concurrency/src/main.rs
+++ chapter_2/i4_concurrency/src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -20,4 +21,26 @@ fn complex_square_add_loop(c: Complex<f64>) {
     loop {
         println!("{}", z = z * z + c)
     }
+}
+
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+/// iterations to decide.
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// iterations it took for `c` to leave the circle of radius two centered on the
+/// origin. If `c` seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that `c` is not a member),
+/// return `None`.
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        z = z * z + c;
+
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+
+    None
 }
