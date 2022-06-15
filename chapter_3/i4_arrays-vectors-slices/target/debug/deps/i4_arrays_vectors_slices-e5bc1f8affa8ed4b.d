368a383bef582a960bedb7d69f82897f4434cec2
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-15 07:48:33 +0100

    removed target folder

--- chapter_3/i4_arrays-vectors-slices/target/debug/deps/i4_arrays_vectors_slices-e5bc1f8affa8ed4b.d
+++
@@ -1,5 +0,0 @@
-/home/alexander/ultron/practi_code/rust_lang/rust_programming/chapter_3/i4_arrays-vectors-slices/target/debug/deps/i4_arrays_vectors_slices-e5bc1f8affa8ed4b.rmeta: src/main.rs
-
-/home/alexander/ultron/practi_code/rust_lang/rust_programming/chapter_3/i4_arrays-vectors-slices/target/debug/deps/i4_arrays_vectors_slices-e5bc1f8affa8ed4b.d: src/main.rs
-
-src/main.rs:
