c119e4e6650574aa5fe56b9b38d403b2cdfcf172
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-15 07:30:52 +0100

    Pointer type 1: References

--- chapter_3/i3_pointer_types/src/main.rs
+++ chapter_3/i3_pointer_types/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+        Three types of pointer types in Rust:
+           1. References
+           2. Boxes
+           3. Unsafe Pointers
+
+        1. References: References are Rust basic pointer type.
+           A References can point to any value anywhere, stack
+           or heap.
+
+           &x -> borrows a reference to x
+           *x -> is the value x points to.
+
+           Rust track the ownership and lifetime of values, so
+           mistakes like dangling pointers, double frees, and
+           pointer invalidation are ruled out at compile time.
+    */
 }
