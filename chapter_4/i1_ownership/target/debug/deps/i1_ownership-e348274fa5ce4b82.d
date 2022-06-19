effecc34fedd8733ba34f2fec021bf0eb17f9767
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-19 17:08:47 +0100

    Revert "Initial commit on ownerships"
    This reverts commit 9cbe8bb824c5f89b10048b12b34fab643a119f4c.

--- chapter_4/i1_ownership/target/debug/deps/i1_ownership-e348274fa5ce4b82.d
+++
@@ -1,5 +0,0 @@
-/home/alexander/ultron/practi_code/rust_lang/rust_programming/chapter_4/i1_ownership/target/debug/deps/i1_ownership-e348274fa5ce4b82.rmeta: src/main.rs
-
-/home/alexander/ultron/practi_code/rust_lang/rust_programming/chapter_4/i1_ownership/target/debug/deps/i1_ownership-e348274fa5ce4b82.d: src/main.rs
-
-src/main.rs:
