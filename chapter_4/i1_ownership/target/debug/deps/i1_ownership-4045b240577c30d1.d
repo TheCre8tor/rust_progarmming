9cbe8bb824c5f89b10048b12b34fab643a119f4c
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-19 17:06:07 +0100

    Initial commit on ownerships

---
+++ chapter_4/i1_ownership/target/debug/deps/i1_ownership-4045b240577c30d1.d
@@ -0,0 +1,5 @@
+/home/alexander/ultron/practi_code/rust_lang/rust_programming/chapter_4/i1_ownership/target/debug/deps/i1_ownership-4045b240577c30d1.rmeta: src/main.rs
+
+/home/alexander/ultron/practi_code/rust_lang/rust_programming/chapter_4/i1_ownership/target/debug/deps/i1_ownership-4045b240577c30d1.d: src/main.rs
+
+src/main.rs:
