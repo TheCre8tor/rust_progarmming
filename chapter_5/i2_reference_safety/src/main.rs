68bde11f5b5ccdff84ad1b203cc2a7f9e5c1cb90
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-21 22:01:32 +0100

    static lifetime

--- chapter_5/i2_reference_safety/src/main.rs
+++ chapter_5/i2_reference_safety/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    /* Rust’s equivalent of a global variable is called
+       a static: it’s a value that’s created when the
+       program starts and lasts until it terminates.
+
+       RULES:
+       1. Every static must be initialized.
+       2. Mutable statics are inherently not thread-safe.
+          -> For these reasons, you may access a mutable
+          static only within an unsafe block.
+    */
+    static mut STASH: &i32 = &128;
+
+    static WORTH_POINTING_AT: i32 = 3;
+
+    fn f(p: &'static i32) {
+        unsafe {
+            STASH = p;
+        }
+    }
+
+    f(&WORTH_POINTING_AT);
 }
