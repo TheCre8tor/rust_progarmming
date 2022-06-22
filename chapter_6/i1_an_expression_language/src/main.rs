f1aba1aa7419d1be120dd78428d18594a6293b75
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-22 15:41:31 +0100

    Come back for this

--- chapter_6/i1_an_expression_language/src/main.rs
+++ chapter_6/i1_an_expression_language/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    /* Expressions have values. Statements don’t.
+     * Rust is what is called an expression language.
+     * This means it follows an older tradition, dating
+     * back to Lisp, where expressions do all the work.
+     */
 }
