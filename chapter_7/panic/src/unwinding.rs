55d1c117243c3a0732741fd52b93620843ae9aa5
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-23 09:55:03 +0100

    chapter_7 is almost read only

--- chapter_7/panic/src/unwinding.rs
+++ chapter_7/panic/src/unwinding.rs
@@ -1,1 +1,9 @@
-pub fn run() {}
+pub fn run() {
+    let crew_share = pirate_share(100, 0);
+    println!("{}", crew_share);
+}
+
+fn pirate_share(total: u64, crew_size: usize) -> u64 {
+    let half = total / 2;
+    half / crew_size as u64
+}
