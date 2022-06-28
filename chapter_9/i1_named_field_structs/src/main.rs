909e3f71e9581d2f4eee6a41d0e7b7e4f62d2cee
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-06-28 10:36:58 +0100

    Skipped structs for now

--- chapter_9/i1_named_field_structs/src/main.rs
+++ chapter_9/i1_named_field_structs/src/main.rs
@@ -1,3 +1,23 @@
+struct GrayscaleMap {
+    pub pixels: Vec<u8>,
+    pub size: (usize, usize),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width = 1024;
+    let height = 576;
+
+    let image = GrayscaleMap {
+        pixels: vec![0; width * height],
+        size: (width, height),
+    };
+
+    assert_eq!(image.size, (1024, 576));
+    assert_eq!(image.pixels.len(), 1024 * 576);
+}
+
+fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
+    assert_eq!(pixels.len(), size.0 * size.1);
+
+    GrayscaleMap { pixels, size }
 }
