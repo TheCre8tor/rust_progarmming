b602a17035fd22322ce3a068c04896a8237e1868
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-07-09 07:58:49 +0100

    Defining and Implementing Traits

---
+++ chapter_11/i2_defining_and_implementing_traits/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::Write;
+
+fn main() {}
+
+struct Broom {
+    name: String,
+    height: u32,
+    health: u32,
+    position: (f32, f32, f32),
+    intent: BroomIntent,
+}
+
+#[derive(Clone, Copy)]
+enum BroomIntent {
+    FetchWater,
+    DumpWater,
+}
+
+type Canvas = Box<dyn Write>;
+
+/// A trait for characters, items, and scenery -
+/// anything in the game world that's visible on screen.
+trait Visible {
+    /// Render this object on the given canvas.
+    fn draw(&self, canvas: &mut Canvas);
+
+    /// Return true if clicking at (x, y) should
+    /// select this object.
+    fn hit_test(&self, x: i32, y: i32);
+}
