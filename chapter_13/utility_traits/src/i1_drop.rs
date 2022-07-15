e909fa4fed01186aeb99fb157a9035601c3631a1
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-07-15 09:29:22 +0100

    Implemented Drop trait

--- chapter_13/utility_traits/src/i1_drop.rs
+++ chapter_13/utility_traits/src/i1_drop.rs
@@ -1,1 +1,37 @@
-pub fn run() {}
+use std::ops::Drop;
+
+struct Appellation {
+    name: String,
+    nicknames: Vec<String>,
+}
+
+impl Drop for Appellation {
+    fn drop(&mut self) {
+        print!("Dropping {}", self.name);
+
+        if !self.nicknames.is_empty() {
+            {
+                print!("(AKA {})", self.nicknames.join(", "));
+            }
+        }
+
+        println!("")
+    }
+}
+
+pub fn run() {
+    let mut a = Appellation {
+        name: "Zeus".to_owned(),
+        nicknames: vec![
+            "cloud collector".to_string(),
+            "king of the gods".to_string(),
+        ],
+    };
+
+    println!("before assingnment");
+    a = Appellation {
+        name: "Hera".to_string(),
+        nicknames: vec![],
+    };
+    println!("at the end of block");
+}
